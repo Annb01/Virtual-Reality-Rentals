14edd22c45d7df113ea80bbb3985f8ce374b42ce
Annb01 <bubula.ann1@gmail.com>
2025-03-23 18:36:53 +0100

    Initial Commit 2

---
+++ VR_Rentals/VR_Rentals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VR_Rentals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
