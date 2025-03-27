42799235a848abee81b8f8bf1200d9f4559c9b64
Annb01 <bubula.ann1@gmail.com>
2025-03-27 19:13:56 +0100

    Create database models and initial migration

---
+++ VR_Rentals/VR_Rentals/Models/VrEquipment.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VR_Rentals.Models
+{
+    public enum TypeEnum
+    {
+        VRHeadset,
+        FullSet,
+        Controllers
+    }
+    public enum Status
+    {
+        Rented,
+        Available
+    }
+    public class VrEquipment
+    {
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public TypeEnum TypeName { get; set; }
+        public string Manufacturer { get; set; }
+        public double PricePerDay { get; set; }
+        public Status EquipmentStatus { get; set; }
+        public bool NeedController { get; set; }
+        public byte[] Image { get; set; }
+        public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+    }
+}
